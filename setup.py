c72f14d5d21bce007380655829e6051b9a00ec7a
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2020-12-21 09:48:11 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name=NotImplemented,
+    version="0.0.1",
+    description=NotImplemented,
+    author="Eduardo Pignatelli",
+    author_email="edu.pignatelli@gmail.com",
+    url=NotImplemented,
+    packages=[],
+    install_requires=open("requirements.txt", "r").readlines(),
+)
