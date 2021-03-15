f367604fa76197d0c10d7cb9e8af4601128118a6
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-03-15 17:02:19 +0000

    fix replay buffer

--- helx/rl.py
+++ helx/rl.py
@@ -1,5 +1,7 @@
 import logging
 from collections import deque
+
+import jax
 import jax.numpy as jnp
 
 from .types import Transition
@@ -9,9 +11,10 @@ class ReplayBuffer:
     def __init__(self, capacity, seed=0):
         # public:
         self.capacity = capacity
-        self.rng = jnp.random.PRNGKey(seed)
+        self.seed = seed
 
         # private:
+        self._rng = jnp.random.PRNGKey(seed)
         self._data = deque(maxlen=capacity)
 
     def __len__(self):
@@ -28,7 +31,7 @@ class ReplayBuffer:
         )
         return
 
-    def sample(self, n):
+    def sample(self, n, rng=None):
         high = len(self) - n
         if high <= 0:
             logging.warning(
@@ -36,7 +39,9 @@ class ReplayBuffer:
                 "Returning all the available elements".format(len(self), n)
             )
             indices = range(len(self))
+        elif rng is None:
+            rng, _ = jax.random.split(self._rng)
         else:
-            indices = jnp.random.randint(self.rng, 0, high, size=n)
+            indices = jnp.random.randint(rng, 0, high, size=n)
 
         return tuple(zip(*(map(lambda idx: self._data[idx], indices))))
