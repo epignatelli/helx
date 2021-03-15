e526510cecc041c44bbc5a674bf6d41896b76025
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-03-15 11:18:31 +0000

    add rl transition type

--- helx/types.py
+++ helx/types.py
@@ -1,4 +1,5 @@
 from typing import Any, Callable, Dict, NamedTuple, Tuple
+import numpy as onp
 import jax.numpy as jnp
 from jax.experimental.optimizers import OptimizerState
 
@@ -14,10 +15,17 @@ InitState = Callable[[], jnp.ndarray]
 class Module(NamedTuple):
     init: Init
     apply: Apply
-    initial_state: InitState = None
 
 
 class Optimiser(NamedTuple):
     init: Callable[[Params], OptimizerState]
     update: Callable[[OptimizerState], OptimizerState]
     params: Callable[[OptimizerState], Params]
+
+
+class Transition(NamedTuple):
+    s: onp.ndarray
+    a: onp.ndarray
+    r: onp.ndarray
+    ns: onp.ndarray
+    na: onp.ndarray = None  #  optional on-policy action
