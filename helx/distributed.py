0b01ce5d07f5cc924298765e2d811fbd4d8a4386
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-04-20 14:22:43 +0100

    expand redistributed

--- helx/distributed.py
+++ helx/distributed.py
@@ -11,6 +11,16 @@ def distribute_tree(tree):
     return jax.tree_map(lambda x: jnp.array([x] * jax.local_device_count()), tree)
 
 
+def gather_tree(tree):
+    """
+    This is usually used to reduce back a tree that has been distributed
+    using `distribute_tree` or `redistribute_tree`.
+    Assumes that the tree is duplicated along the axis 0 an arbitrary number of times,
+    and selects only one of the replicas of the pyTree
+    """
+    return jax.tree_map(lambda x: x[0], tree)
+
+
 def redistribute_tree(tree):
     """
     This is usually used to prepare the params for `pmap`ped functions when the params
@@ -21,17 +31,7 @@ def redistribute_tree(tree):
     This function sets the number of duplicates equal to the
      number returned by `jax.local_device_count()`.
     """
-    return jax.tree_map(lambda x: jnp.array([x[0]] * jax.local_device_count()), tree)
-
-
-def gather_tree(tree):
-    """
-    This is usually used to reduce back a tree that has been distributed
-    using `distribute_tree` or `redistribute_tree`.
-    Assumes that the tree is duplicated along the axis 0 an arbitrary number of times,
-    and selects only one of the replicas of the pyTree
-    """
-    return jax.tree_map(lambda x: x[0], tree)
+    return distribute_tree(gather_tree(tree))
 
 
 def distribute_array(array):
@@ -45,9 +45,28 @@ def distribute_array(array):
     n = jax.local_device_count()
     s = array.shape
     assert (
-        len(s) >= 2 and s[0] % n == 0
+        len(s) > 1 and s[0] % n == 0
     ), "Array dimension {} must be divisible for the number of local devices {}".format(
         s[0], n
     )
     new_shape = (n,) + (s[0] // n,) + s[1:]
     return array.reshape(new_shape)
+
+
+def gather_array(array):
+    """
+    Gathers an array that has previously been distributed using `distribute_array`,
+    by removing the leading axis and stacking them on the second axis.
+    """
+    s = array.shape
+    assert len(s) > 2, "Array must have at least 3 axes"
+    return array.reshape(s[0] + s[1], s[2:])
+
+
+def redistribute_array(array):
+    """
+    Gathers and then distributes again a given array.
+    See `helx.distributed.distribute_array` and `helx.distributed.gather_array`
+    for more
+    """
+    return distribute_array(gather_array(array))
