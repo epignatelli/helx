e3480b27cb13c84dc8b118430cb5a1155fac80c8
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-05-05 09:39:32 +0100

    switch to typevar

--- helx/typing.py
+++ helx/typing.py
@@ -1,20 +1,23 @@
 import functools
-from typing import Any, Callable, Dict, NamedTuple, Tuple
+from typing import Any, Callable, Dict, NamedTuple, Tuple, TypeVar
 
 import jax.numpy as jnp
 
-
-Key = jnp.ndarray
-Shape = Tuple[int, ...]
-Size = Tuple[int, int]
-Params = Any
+Key = TypeVar("Key", jnp.ndarray)
+Shape = TypeVar("Shape", Tuple[int, ...])
+Size = TypeVar("Size", Tuple[int, int])
+Params = TypeVar("Params", Any)
+# Init = TypeVar("Init", Callable[[Key, Shape], Tuple[Shape, Params]])
+# Error: TypeVar bound type cannot be generic. TODO(ep): add when generics are supported
 Init = Callable[[Key, Shape], Tuple[Shape, Params]]
+# Apply = TypeVar("Apply", Callable[[Params, jnp.ndarray, Dict], jnp.ndarray])
+# Error: TypeVar bound type cannot be generic.
 Apply = Callable[[Params, jnp.ndarray, Dict], jnp.ndarray]
-HParams = NamedTuple
-Logits = jnp.ndarray
-Value = jnp.ndarray
-Loss = float
-Return = float
+HParams = TypeVar("HParams", NamedTuple)
+Logits = TypeVar("Logits", jnp.ndarray)
+Value = TypeVar("Value", jnp.ndarray)
+Loss = TypeVar("Loss", float)
+Return = TypeVar("Return", float)
 
 
 def factory(cls_maker, T):
