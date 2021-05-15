762f7ce459c8be4081fba633a3409ac22e4a094c
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-05-15 15:13:31 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="Helx",
-    version="0.0.2.0",
+    version="0.0.3.0",
     description="Helx is a helper library for JAX/stax",
     author="Eduardo Pignatelli",
     author_email="edu.pignatelli@gmail.com",
