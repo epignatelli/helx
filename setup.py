2c76607e4b5ebdd875c622db1a533ad3c4967c57
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-05-20 12:54:13 +0100

    fix setup'

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@
 
 from distutils.core import setup
 
+
+def parse_requirements(filename):
+    #  TODO(ep): parse git-based repositories
+    return open(filename, "r").readlines()
+
+
 setup(
     name="Helx",
     version="0.0.3.0",
-    description="Helx is a helper library for JAX/stax",
+    description="Helx is a helper library for JAX/stax to implement \
+                 Reinforcement Learning and Deep Learning algorithms",
     author="Eduardo Pignatelli",
     author_email="edu.pignatelli@gmail.com",
     url="https://github.com/epignatelli/helx",
     packages=["helx"],
-    install_requires=open("requirements.txt", "r").readlines(),
+    install_requires=parse_requirements("requirements.txt"),
 )
