6ccd0822d9cbc3403348bb2416645f1b5e5ed9f5
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-05-20 13:10:48 +0100

    fix imports

--- helx/nn/module.py
+++ helx/nn/module.py
@@ -3,11 +3,9 @@ from typing import Callable, NamedTuple, Tuple
 
 import jax
 import jax.numpy as jnp
-from jax.experimental.optimizers import OptimizerState
 
-from ..typing import Apply, Init, Params, factory
+from ..typing import Apply, Init, factory
 from ..distributed import distribute_tree
-from ..optimise.optimisers import Optimiser
 
 
 InitState = Callable[[], jnp.ndarray]
@@ -44,33 +42,3 @@ def pmodule(fun):
         return module._replace(init=init)
 
     return wrapper
-
-
-def nn(forward_fun, **kwargs):
-    @functools.partial(jax.jit, static_argnums=0)
-    def backward(
-        model: Module, params: Params, *args, **kwargs
-    ) -> Tuple[jnp.ndarray, jnp.ndarray]:
-        return jax.value_and_grad(forward_fun, argnums=1, has_aux=True)(
-            model, params, *args, **kwargs
-        )
-
-    @functools.partial(jax.jit, static_argnums=(0, 1))
-    def sgd_step(
-        model: Module,
-        optimiser: Optimiser,
-        iteration: int,
-        optimiser_state: OptimizerState,
-        *args,
-        **kwargs
-    ) -> Tuple[float, jnp.ndarray, OptimizerState]:
-        params = optimiser.params(optimiser_state)
-        (loss, y_hat), gradients = backward(model, params, *args, **kwargs)
-        return loss, y_hat, optimiser.update(iteration, gradients, optimiser_state)
-
-    cls = forward_fun.__globals__[forward_fun.__qualname__.split(".")[0]]
-    forward_jit = jax.jit(forward_fun, **kwargs)
-    setattr(cls, forward_fun.__name__, staticmethod(forward_jit))
-    setattr(cls, backward.__name__, staticmethod(backward))
-    setattr(cls, sgd_step.__name__, staticmethod(sgd_step))
-    return nn
