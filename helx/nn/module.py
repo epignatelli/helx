f810a22cc83f0f2d297fdbacf76ae51d80e3e2a4
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-06-08 12:42:38 +0100

    elementwise ops on modules

--- helx/nn/module.py
+++ helx/nn/module.py
@@ -1,7 +1,11 @@
 import functools
 from typing import Callable, NamedTuple
+from jax._src.numpy.lax_numpy import ndarray
 
+from jax.experimental.stax import serial, elementwise
 import jax.numpy as jnp
+from jax import lax
+
 
 from helx.typing import Apply, Init, factory
 from helx.distributed import distribute_tree
@@ -10,9 +14,51 @@ from helx.distributed import distribute_tree
 InitState = Callable[[], jnp.ndarray]
 
 
+operator = lambda op, y: elementwise(functools.partial(op, y=y))
+
+
 class Module(NamedTuple):
     init: Init
     apply: Apply
+
+    def __add__(self, y):
+        return serial(self, operator(lax.add, y))
+
+    def __sum__(self, y):
+        return serial(self, operator(lax.sub, y))
+
+    def __mul__(self, y: jnp.ndarray):
+        return serial(self, operator(lax.mul, y))
+
+    def __floordiv__(self, y: jnp.ndarray):
+        return serial(self, operator(lax.div, y))
+
+    def __truediv__(self, y: jnp.ndarray):
+        return serial(self, operator(lax.div, y))
+
+    def __matmul__(self, y: jnp.ndarray):
+        return serial(self, elementwise(functools.partial(lax.batch_matmul, rhs=y)))
+
+    def __pow__(self, y: jnp.ndarray):
+        return serial(self, operator(lax.pow, y))
+
+    def __eq__(self, y: object):
+        return serial(self, operator(lax.eq, y))
+
+    def __ne__(self, y: object):
+        return serial(self, operator(lax.ne, y))
+
+    def __gt__(self, y: object):
+        return serial(self, operator(lax.gt, y))
+
+    def __ge__(self, y: object):
+        return serial(self, operator(lax.ge, y))
+
+    def __lt__(self, y: object):
+        return serial(self, operator(lax.lt, y))
+
+    def __le__(self, y: object):
+        return serial(self, operator(lax.le, y))
 
 
 def module(fun):
