5e997ef63224e8f908eff2da82f4062f6bb3f281
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2021-06-10 10:44:56 +0100

    add impala baseline

--- helx/rl/agent.py
+++ helx/rl/agent.py
@@ -1,11 +1,16 @@
 import abc
 import logging
+from typing import Callable, Sequence
 
 import dm_env
-
+import jax
+import jax.numpy as jnp
 from helx.nn.module import Module
 from helx.optimise.optimisers import Optimiser
-from helx.typing import Action, HParams, Loss
+from helx.typing import Action, Batch, HParams, Loss, Params
+from jaxlib.xla_extension import Device
+
+Policy = Callable
 
 
 class IAgent:
@@ -97,3 +102,63 @@ class IAgent:
                 # prepare next iteration
                 timestep = new_timestep
         return loss
+
+
+class Actor:
+    def __init__(
+        self,
+        policy: Policy,
+        params: Params,
+        env: dm_env.Environment,
+        n_steps: int = 1,
+        device: Device = None,
+    ) -> None:
+        #  public:
+        self.env: dm_env.Environment = env
+        self.policy: Callable = policy
+        self.n_steps: int = n_steps
+        self.trajectory: Sequence[dm_env.TimeStep] = [self.reset()]
+
+        #  private:
+        self._device: Device = device if device is not None else jax.local_devices()[0]
+        self._params: Params = params
+        self._policy_distribution = []
+
+    @property
+    def params(self):
+        return self._params
+
+    @property
+    def device(self):
+        return self._device
+
+    @property
+    def policy_distribution(self):
+        return self._policy_distribution
+
+    def update(self, params: Params):
+        self.params = params
+
+    def send(self):
+        return (self.trajectory, self._params)
+
+    def step(self, policy):
+        #  apply policy
+        policy_distribution = policy(self.trajectory[-1].observation)
+        self._policy_distribution.append(policy_distribution)
+        action = jnp.argmax(policy_distribution, axis=-1)
+        #  step in the environment
+        new_timestep = self.env.step(action)
+        #  store experience
+        self.trajectory.append(new_timestep)
+        return new_timestep
+
+    def reset(self):
+        return self.env.reset()
+
+
+class Learner:
+    def __init__(self) -> None:
+        self.experience_queue =
+
+    def update(self, trajectories: Batch[Sequence[dm_env.TimeStep]]):
