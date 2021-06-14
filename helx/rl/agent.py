471c2ba74c6fb73fe990a75258b2b55203a11168
Eduardo <eduardo.pignatelli@burohappold.com>
2021-06-14 22:06:27 +0100

    remove bas implementation of parallelised RL

--- helx/rl/agent.py
+++ helx/rl/agent.py
@@ -1,15 +1,11 @@
 import abc
 import logging
-from typing import Callable, NamedTuple, Sequence
-from multiprocessing import Process
+from typing import Callable
 
 import dm_env
-import jax
-import jax.numpy as jnp
 from helx.nn.module import Module
 from helx.optimise.optimisers import Optimiser
-from helx.typing import Action, Batch, HParams, Loss, Params
-from jaxlib.xla_extension import Device
+from helx.typing import Action, HParams, Loss
 
 Policy = Callable
 
@@ -104,94 +100,12 @@ class IAgent:
                 timestep = new_timestep
         return loss
 
-    def run_distributed(
+    def run_async(
         self,
         env: dm_env.Environment,
-        n_actors: int = 1,
-        n_learners: int = 1,
         num_episodes: int = 1000,
         eval: bool = False,
     ):
         """Start the training routine of a Reinforcement Learning agent
         using multiple instances of the environment to increase throughput"""
-
-        envs = ParallelEnv(env, n=n_actors)
-
-        def act(experience_buffer, policy_buffer):
-            pass
-
-        def learn(experience_buffer, policy_buffer):
-            pass
-
-        experience_buffer = None
-        policy_buffer = PolicyBuffer()
-
-        actors = Process(
-            target=act,
-            args=(experience_buffer, policy_buffer),
-            daemon=True,
-            name="actors",
-        )
-
-
-class PolicyBuffer(NamedTuple):
-    policy: Params = None
-
-    def get(self):
-        return self.policy
-
-    def put(self, policy):
-        self.policy = policy
-
-
-class Actor:
-    def __init__(
-        self,
-        policy: Policy,
-        params: Params,
-        env: dm_env.Environment,
-        n_steps: int = 1,
-        device: Device = None,
-    ) -> None:
-        #  public:
-        self.env: dm_env.Environment = env
-        self.policy: Callable = policy
-        self.n_steps: int = n_steps
-        self.trajectory: Sequence[dm_env.TimeStep] = [self.reset()]
-
-        #  private:
-        self._device: Device = device if device is not None else jax.local_devices()[0]
-        self._params: Params = params
-        self._policy_distribution = []
-
-    @property
-    def params(self):
-        return self._params
-
-    @property
-    def device(self):
-        return self._device
-
-    @property
-    def probs(self):
-        return self._probs
-
-    def update(self, params: Params):
-        self.params = params
-
-    def send(self):
-        return (self.trajectory, self._params)
-
-    def step(self, policy):
-        #  apply policy
-        probs = policy(self.trajectory[-1].observation)
-        self._probs.append(probs)
-        action = jnp.argmax(probs, axis=-1)
-        #  step in the environment
-        new_timestep = self.env.step(action)
-        #  store experience
-        self.trajectory.append(new_timestep)
-        return new_timestep
-
-    def reset(self):
-        return self.env.reset()
+        raise NotImplementedError("Asynchronous actors are not implemented.")
