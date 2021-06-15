7245b6e7040b7910fd19ad7ec4a476bc50cdfafa
Eduardo <eduardo.pignatelli@burohappold.com>
2021-06-15 15:58:18 +0100

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def parse_requirements(filename):
 
 setup(
     name="Helx",
-    version="0.0.3.8",
+    version="0.0.4.0",
     description="Helx is a helper library for JAX/stax to implement \
                  Reinforcement Learning and Deep Learning algorithms",
     author="Eduardo Pignatelli",
