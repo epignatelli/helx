fix l1 norm calculation

experiments/sa/impala/util.py
@@ -83,6 +83,6 @@ class WandbLogger(AbslLogger):
 
 
 def l1_norm(tree):
-    """Compute the l2 norm of a pytree of arrays. Useful for weight decay."""
+    """Compute the l1 norm of a pytree of arrays. Useful for weight decay."""
     leaves, _ = jax.tree_flatten(tree)
-    return jnp.abs(sum(jnp.vdot(x, x) for x in leaves))
+    return sum(jnp.linalg.norm(x, ord=1) for x in leaves)