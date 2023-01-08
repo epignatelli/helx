3a11f901ee05a78898af0b3b5c70833ae1f0eab6
“Eduardo <edu.pignatelli@gmail.com>
2023-01-08 15:35:02 +0100

    fix env name

--- helx/environment/gym.py
+++ helx/environment/gym.py
@@ -73,3 +73,6 @@ class FromGymEnv(Environment[gym.Env]):
 
     def close(self) -> None:
         return self._env.close()
+
+    def name(self) -> str:
+        return self._env.unwrapped.__class__.__name__
