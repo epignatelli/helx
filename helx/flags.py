bd55da824ff460848265bb151f6ce2b8813dd6dd
“Eduardo <edu.pignatelli@gmail.com>
2023-01-08 15:05:56 +0100

    add flags for spaces and make mandary arguments as inputs

--- helx/flags.py
+++ helx/flags.py
@@ -1,13 +1,16 @@
 import dataclasses
 from typing import Sequence, get_type_hints
-from absl import flags
-from absl.flags import _flagvalues, _argument_parser, DEFINE
+
 import chex
+from absl import flags
+from absl.flags import DEFINE, _argument_parser, _flagvalues
+
+from .spaces import Space, Continuous, Discrete
 
 
 class ShapeParser(_argument_parser.ListParser):
-    def parse(self, arg):
-        return tuple(map(int, super().parse(arg)))
+    def parse(self, shape: chex.Shape):
+        return tuple(map(int, super().parse(shape)))
 
 
 def DEFINE_shape(  # pylint: disable=invalid-name,redefined-builtin
@@ -40,6 +43,47 @@ def DEFINE_shape(  # pylint: disable=invalid-name,redefined-builtin
     )
 
 
+class SpaceParser(_argument_parser.ListParser):
+    def parse(self, space: Space):
+        shape = tuple(map(int, super().parse(space.shape)))
+        dtype = space.dtype
+        if shape != (1,):
+            space = Continuous(shape, dtype)
+        else:
+            space = Discrete(1)
+        return (shape, space.dtype)
+
+
+def DEFINE_space(  # pylint: disable=invalid-name,redefined-builtin
+    name, default, help, flag_values=_flagvalues.FLAGS, required=False, **args
+):
+    """Registers a flag whose value is a comma-separated list of dimensions.
+
+    The flag value is parsed with a CSV parser.
+
+    It is the same as a ListParser, but the output is a tuple of integers.
+
+    Args:
+      name: str, the flag name.
+      default: list|str|None, the default value of the flag.
+      help: str, the help message.
+      flag_values: :class:`FlagValues`, the FlagValues instance with which the
+        flag will be registered. This should almost never need to be overridden.
+      required: bool, is this a required flag. This must be used as a keyword
+        argument.
+      **args: Dictionary with extra keyword args that are passed to the
+        ``Flag.__init__``.
+
+    Returns:
+      a handle to defined flag.
+    """
+    parser = SpaceParser()
+    serializer = _argument_parser.CsvListSerializer(",")
+    return DEFINE(
+        parser, name, default, help, flag_values, serializer, required=required, **args
+    )
+
+
 def type_to_flag(type_):
     if type_ == bool:
         return flags.DEFINE_boolean
@@ -51,15 +95,29 @@ def type_to_flag(type_):
         return flags.DEFINE_string
     elif type_ == chex.Shape:
         return DEFINE_shape
+    elif type_ == Space:
+        return DEFINE_space
     elif type_ in (list, Sequence, tuple):
         return flags.DEFINE_list
     else:
         raise ValueError("Type {} is not supported".format(type_))
 
 
+def get_hparams_fields(type_):
+    fields = get_type_hints(type_)
+    if "obs_space" in fields:
+        del fields["obs_space"]
+    if "action_space" in fields:
+        del fields["action_space"]
+    return fields
+
 def define_flags_from_hparams(type_):
     # get_type_hints to get precise types than annotations
-    fields = get_type_hints(type_)
+    fields = get_hparams_fields(type_)
+    if "obs_space" in fields:
+        del fields["obs_space"]
+    if "action_space" in fields:
+        del fields["action_space"]
 
     # we wrap the type in a dataclass to get the default values
     dataclass_fields = dataclasses.dataclass(type_).__dataclass_fields__
@@ -80,16 +138,19 @@ def define_flags_from_hparams(type_):
     return
 
 
-def hparams_from_flags(cls, flags, **kwargs):
+def hparams_from_flags(
+    cls, obs_space: Space, action_space: Space, flags=flags.FLAGS, **kwargs
+):
     hparams = {}
     for key, value in kwargs.items():
         hparams[key] = value
-    for field in get_type_hints(cls):
+    for field in get_hparams_fields(cls):
         if field in kwargs:
             continue
         value = flags[field].value
         if value is None:
             raise ValueError("Flag {} is required".format(field))
         hparams[field] = value
-
+    hparams["obs_space"] = obs_space
+    hparams["action_space"] = action_space
     return cls(**hparams)
