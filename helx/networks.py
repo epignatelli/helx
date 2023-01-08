3f368d1ae4a0f78cf5e77f898e27e4abef183647
“Eduardo <edu.pignatelli@gmail.com>
2023-01-08 15:07:44 +0100

    add docs to networks and add policy heads

--- helx/networks.py
+++ helx/networks.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from functools import wraps
-from typing import Sequence, Tuple
+from typing import NamedTuple, Sequence, Tuple
+import distrax
 
 import flax.linen as nn
 import jax
@@ -14,170 +15,56 @@ from .mdp import Action
 
 @wraps(jax.jit)
 def jit_bound(method, *args, **kwargs):
+    """A wrapper for jax.jit that allows jitting bound methods, such as
+    instance methods."""
     return jax.jit(nn.module._get_unbound_fn(method), *args, **kwargs)
 
 
 @wraps(optax.apply_updates)
 def apply_updates(params: nn.FrozenDict, updates: optax.Updates) -> nn.FrozenDict:
-    # work around a typing compatibility issue
-    # optax.apply_updates expects a pytree of parameters
-    # but works with nn.FrozenDict anyway
+    """Applies updates to parameters using optax. This is a workaround for a typing
+    compatibility issue between optax and Flax: optax.apply_updates expects a pytree
+    of parameters, but works with nn.FrozenDict anyway.
+    Args:
+        params (nn.FrozenDict): A pytree of Flax parameters.
+        updates (optax.Updates): A pytree of updates.
+    Returns:
+        nn.FrozenDict: The update pytree of Flax parameters.
+    """
     return optax.apply_updates(params, updates)  # type: ignore
 
 
 class Flatten(nn.Module):
+    """A Flax module that flattens the input array."""
+
     @nn.compact
     def __call__(self, x: Array) -> Array:
         return x.flatten()
 
 
 class Identity(nn.Module):
+    """A Flax module that returns the input array."""
+
     @nn.compact
     def __call__(self, x: Array) -> Array:
         return x
 
 
-class AgentNetwork(nn.Module):
-    actor_net: nn.Module | None = None
-    critic_net: nn.Module | None = None
-    state_transition_net: nn.Module | None = None
-    reward_net: nn.Module | None = None
-    extra_net: nn.Module | None = None
-    state_representation_net: nn.Module = Identity()
-
-    @nn.compact
-    def __call__(
-        self, observation: Array, *args, **kwargs
-    ) -> Tuple[Array, Array, Array, Array, PyTreeDef]:
-        modules = [
-            self.actor_net,
-            self.critic_net,
-            self.state_transition_net,
-            self.reward_net,
-            self.extra_net,
-        ]
-        representation = self.state_representation_net(observation)
-        ys = []
-        for module in modules:
-            if module is None:
-                y = jnp.empty((0,))
-            else:
-                y = module(representation, *args, **kwargs)
-            ys.append(y)
-        return tuple(ys)
-
-    def state_representation(
-        self, params: nn.FrozenDict, observation: Array, **kwargs
-    ) -> Array:
-        return jnp.asarray(
-            self.state_representation_net.apply(
-                {"params": params["params"]["representation_net"]},
-                observation,
-                **kwargs,
-            )
-        )
-
-    def actor(
-        self,
-        params: nn.FrozenDict,
-        observation: Array,
-        apply_representation=False,
-        **kwargs,
-    ) -> Array:
-        if self.actor_net is None:
-            raise ValueError("Actor net not defined")
-        if apply_representation:
-            observation = self.state_representation(params, observation, **kwargs)
-        return jnp.asarray(
-            self.actor_net.apply(
-                {"params": params["params"]["actor_net"]},
-                observation,
-                **kwargs,
-            )
-        )
-
-    def critic(
-        self,
-        params: nn.FrozenDict,
-        observation: Array,
-        apply_representation=False,
-        **kwargs,
-    ) -> Array:
-        if self.critic_net is None:
-            raise ValueError("Critic net not defined")
-        if apply_representation:
-            observation = self.state_representation(params, observation, **kwargs)
-        return jnp.asarray(
-            self.critic_net.apply(
-                {"params": params["params"]["critic_net"]},
-                observation,
-                **kwargs,
-            )
-        )
-
-    def state_transition(
-        self,
-        params: nn.FrozenDict,
-        observation: Array,
-        action: Action,
-        apply_representation=False,
-        **kwargs,
-    ) -> Array:
-        if self.state_transition_net is None:
-            raise ValueError("State transition net not defined")
-        if apply_representation:
-            observation = self.state_representation(params, observation, **kwargs)
-        return jnp.asarray(
-            self.state_transition_net.apply(
-                {"params": params["params"]["state_transition_net"]},
-                observation,
-                action,
-                **kwargs,
-            )
-        )
-
-    def reward(
-        self,
-        params: nn.FrozenDict,
-        observation: Array,
-        action: Action,
-        apply_representation=False,
-        **kwargs,
-    ) -> Array:
-        if self.reward_net is None:
-            raise ValueError("Reward net not defined")
-        if apply_representation:
-            observation = self.state_representation(params, observation, **kwargs)
-        return jnp.asarray(
-            self.reward_net.apply(
-                {"params": params["params"]["reward_net"]},
-                observation,
-                action,
-                **kwargs,
-            )
-        )
-
-    def extra(
-        self,
-        params: nn.FrozenDict,
-        observation: Array,
-        apply_representation=False,
-        **kwargs,
-    ) -> Array:
-        if self.extra_net is None:
-            raise ValueError("Extra net not defined")
-        if apply_representation:
-            observation = self.state_representation(params, observation, **kwargs)
-        return jnp.asarray(
-            self.extra_net.apply(
-                {"params": params["params"]["extra_net"]},
-                observation,
-                **kwargs,
-            )
-        )
-
-
 class MLP(nn.Module):
+    """Defines a multi-layer perceptron by alternating a dense layer with a ReLU
+    non-linearity.
+    Args:
+        features (Sequence[int]): A sequence of integers that define the number of
+            units in each dense layer. E.g., (32, 64) defines a network with two
+            dense layers, the first with 32 units and the second with 64.
+    Example:
+        >>> import jax.numpy as jnp
+        >>> from helx.networks import MLP
+        >>> mlp = MLP(features=(32, 64))
+        >>> x = jnp.ones((1, 28, 28))
+        >>> mlp(x).shape
+        (1, 64)"""
+
     features: Sequence[int]
 
     @nn.compact
@@ -189,7 +76,34 @@ class MLP(nn.Module):
 
 
 class CNN(nn.Module):
-    features: Tuple[int] = (32,)
+    """Defines a convolutional neural network by alternating a
+    convolution with a ReLU non-linearity.
+    There is no pooling or other non-linearities between convolutions.
+    Args:
+        features (Tuple[int, ...]): A sequence of integers that define the number of
+            filters in each convolution. E.g., (32, 64) defines a network with
+            two convolutions, the first with 32 filters and the second with 64.
+        kernel_sizes (Tuple[Tuple[int, ...]]): A sequence of tuples that define
+            the kernel size of each convolution.
+        strides (Tuple[Tuple[int, ...]]): A sequence of tuples that define the
+            stride of each convolution.
+        padding (Tuple[nn.linear.PaddingLike]): A sequence of padding modes for
+            each convolution.
+
+    Example:
+        >>> import jax.numpy as jnp
+        >>> from helx.networks import CNN
+        >>> cnn = CNN(
+        ...     features=(32, 64),
+        ...     kernel_sizes=((3, 3), (3, 3)),
+        ...     strides=((1, 1), (1, 1)),
+        ...     padding=("SAME", "SAME"),
+        ... )
+        >>> cnn(jnp.ones((1, 10, 10, 3))).shape
+        (1, 10, 10, 64)
+    """
+
+    features: Tuple[int, ...] = (32,)
     kernel_sizes: Tuple[Tuple[int, ...]] = ((3, 3),)
     strides: Tuple[Tuple[int, ...]] = ((1, 1),)
     padding: Tuple[nn.linear.PaddingLike] = ("SAME",)
@@ -223,11 +137,245 @@ class CNN(nn.Module):
         return x
 
 
+class PolicyHead(nn.Module):
+    """Defines the policy head of an agent.
+    Args:
+        n_variables (int): The number of variables in the policy, e.g., the number
+            of actions in a discrete policy, or the 2 variables in a
+            Gaussian policy, $\\mu$ and $\\log(\\sigma)$.
+        action_size (int): The size of the action space, e.g., the number of
+            discrete actions, or the dimensionality of each action
+            in a continuous action space.
+
+        Example:
+            >>> env = gym.make("CarRacing-v2")
+            >>> action_size = env.action_space.shape[0]
+            >>> policy_distr = distrax.Normal(loc=0., scale=1.)
+            >>> n_variables = 2  # mu, log(sigma)
+            >>> policy_head = PolicyHead(n_variables=n_variables, action_size=action_size)"""
+
+    n_variables: int
+    action_size: int
+
+    @nn.compact
+    def __call__(self, x: Array) -> Tuple[Array, ...]:
+        ys = []
+        for _ in range(self.n_variables):
+            y = nn.Dense(features=self.action_size)(x)
+            ys.append(y)
+        return tuple(ys)
+
+
+class CriticHead(nn.Module):
+    """Defines the critic head of an agent (i.e., the value function).
+    Args:
+        n_actions (int): The number of actions in the action space."""
+
+    n_actions: int
+
+    @nn.compact
+    def __call__(self, x: Array) -> Array:
+        y = nn.Dense(features=self.n_actions)(x)
+        return y
+
+
+class GaussianPolicy(nn.Module):
+    action_size: int
+
+    @nn.compact
+    def __call__(self, observation, key):
+        mu = nn.Dense(features=self.action_size)(observation)
+        log_sigma = nn.Dense(features=self.action_size)(observation)
+        # reparametrisation trick
+        noise = jax.random.normal(key, (self.hparams.dim_A,))
+        action = jnp.tanh(mu + log_sigma * noise)
+        log_prob = distrax.Normal(loc=mu, scale=jnp.exp(log_sigma)).log_prob(action)
+        return action, log_prob
+
+
+class AgentPrediction(NamedTuple):
+    """Defines the most general output of an agent, including both
+    model-free, model-based, and self-motivated agents."""
+
+    preferences: Action
+    values: Array
+    next_state: Array
+    reward: Array
+    extra: Array | PyTreeDef
+    state_representation: Array
+
+
+class AgentNetwork(nn.Module):
+    """Defines the network architecture of an agent, and can be used as it is.
+    Args:
+        actor_net (nn.Module): A Flax module that defines the actor network.
+            The input signature of this module should be `(observation, rng)`.
+        critic_net (nn.Module): A Flax module that defines the critic network.
+        state_transition_net (nn.Module): A Flax module that defines the
+            state-transition dynamics network. Used for model-based RL.
+        reward_net (nn.Module): A Flax module that defines the reward network.
+            Used for model-based RL.
+        extra_net (nn.Module): A Flax module that computes custom, unstructured data
+            (e.g. a log of the agent's actions, additional rewards, goals).
+            The __call__ method of this module should receive at least the
+            observation and the action as inputs, and return a PyTreeDef.
+        state_representation_net (nn.Module): A Flax module that defines the state
+            representation network. If the representation is shared between the actor
+            and critic, then this module should be an instance of `Identity`, and
+            the input of actor_net and critic_net should be the raw observation.
+    """
+
+    actor_net: nn.Module | None = None
+    critic_net: nn.Module | None = None
+    state_transition_net: nn.Module | None = None
+    reward_net: nn.Module | None = None
+    extra_net: nn.Module | None = None
+    state_representation_net: nn.Module = Identity()
+
+    @nn.compact
+    def __call__(
+        self, observation: Array, action: Action, *args, **kwargs
+    ) -> Tuple[Array, Array, Array, Array, Array | PyTreeDef, Array]:
+        representation = self.state_representation_net(observation)
+
+        actor_out = jnp.empty((0,))
+        if self.actor_net is not None:
+            actor_out = self.actor_net(representation, *args, **kwargs)
+
+        critic_out = jnp.empty((0,))
+        if self.critic_net is not None:
+            critic_out = self.critic_net(representation, *args, **kwargs)
+
+        state_transition_out = jnp.empty((0,))
+        if self.state_transition_net is not None:
+            state_transition_out = self.state_transition_net(
+                representation, action, *args, **kwargs
+            )
+
+        reward_out = jnp.empty((0,))
+        if self.reward_net is not None:
+            reward_out = self.reward_net(representation, action, *args, **kwargs)
+
+        extra_out = jnp.empty((0,))
+        if self.extra_net is not None:
+            extra_out = self.extra_net(observation, action, *args, **kwargs)
+
+        return AgentPrediction(
+            actor_out,
+            critic_out,
+            state_transition_out,
+            reward_out,
+            extra_out,
+            representation,
+        )
+
+    def state_representation(
+        self, params: nn.FrozenDict, observation: Array, **kwargs
+    ) -> Array:
+        if "state_representation_net" not in params["params"]:
+            return observation
+        return jnp.asarray(
+            self.state_representation_net.apply(
+                {"params": params["params"]["state_representation_net"]},
+                observation,
+                **kwargs,
+            )
+        )
+
+    def actor(
+        self,
+        params: nn.FrozenDict,
+        observation: Array,
+        **kwargs,
+    ) -> Array:
+        if self.actor_net is None:
+            raise ValueError("Actor net not defined")
+        observation = self.state_representation(params, observation, **kwargs)
+        return jnp.asarray(
+            self.actor_net.apply(
+                {"params": params["params"]["actor_net"]},
+                observation,
+                **kwargs,
+            )
+        )
+
+    def critic(
+        self,
+        params: nn.FrozenDict,
+        observation: Array,
+        **kwargs,
+    ) -> Array:
+        if self.critic_net is None:
+            raise ValueError("Critic net not defined")
+        observation = self.state_representation(params, observation, **kwargs)
+        return jnp.asarray(
+            self.critic_net.apply(
+                {"params": params["params"]["critic_net"]},
+                observation,
+                **kwargs,
+            )
+        )
+
+    def state_transition(
+        self,
+        params: nn.FrozenDict,
+        observation: Array,
+        action: Action,
+        **kwargs,
+    ) -> Array:
+        if self.state_transition_net is None:
+            raise ValueError("State transition net not defined")
+        observation = self.state_representation(params, observation, **kwargs)
+        return jnp.asarray(
+            self.state_transition_net.apply(
+                {"params": params["params"]["state_transition_net"]},
+                observation,
+                action,
+                **kwargs,
+            )
+        )
+
+    def reward(
+        self,
+        params: nn.FrozenDict,
+        observation: Array,
+        action: Action,
+        **kwargs,
+    ) -> Array:
+        if self.reward_net is None:
+            raise ValueError("Reward net not defined")
+        observation = self.state_representation(params, observation, **kwargs)
+        return jnp.asarray(
+            self.reward_net.apply(
+                {"params": params["params"]["reward_net"]},
+                observation,
+                action,
+                **kwargs,
+            )
+        )
+
+    def extra(
+        self,
+        params: nn.FrozenDict,
+        *args,
+        **kwargs,
+    ) -> Array | PyTreeDef:
+        if self.extra_net is None:
+            raise ValueError("Extra net not defined")
+        return jnp.asarray(
+            self.extra_net.apply(
+                {"params": params["params"]["extra_net"]},
+                *args,
+                **kwargs,
+            )
+        )
+
+
 class Temperature(nn.Module):
     initial_temperature: float = 1.0
 
     @nn.compact
-    def __call__(self) -> Array:
+    def __call__(self, observation: Array, action: Array) -> Array:
         log_temperature = self.param(
             "log_temperature",
             init_fn=lambda key: jnp.full((), jnp.log(self.initial_temperature)),
