f7165cbf8d2b50e6c198e8045a8178703d26b984
epignatelli <edu.pignatelli@gmail.com>
2023-01-11 13:00:08 +0000

    add gum3 env

--- helx/environment/interop.py
+++ helx/environment/interop.py
@@ -3,19 +3,25 @@ from typing import Any
 import bsuite.environments
 import dm_env
 import gym.core
+import gym3.interop
 import gymnasium.core
 
 from .bsuite import FromBsuiteEnv
 from .dm_env import FromDmEnv
 from .gym import FromGymEnv
 from .gymnasium import FromGymnasiumEnv
+from .gym3 import FromGym3Env
 
 
 def make_from(env: Any) -> Any:
+    while hasattr(env, "unwrapped") and env.unwrapped != env:
+        env = env.unwrapped
     if isinstance(env, gymnasium.core.Env):
         return FromGymnasiumEnv(env)
     elif isinstance(env, gym.core.Env):
         return FromGymEnv(env)
+    elif isinstance(env, gym3.interop.ToGymEnv):
+        return FromGym3Env(env)
     elif isinstance(env, dm_env.Environment):
         return FromDmEnv(env)
     elif isinstance(env, bsuite.environments.Environment):
