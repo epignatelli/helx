035481b50b4141d4896b0ebe37867d92ac3cdaef
“Eduardo <edu.pignatelli@gmail.com>
2023-01-28 21:11:50 +0000

    remove automatic download of binaries and add it as console script

--- helx/download.py
+++ helx/download.py
@@ -1,3 +1,4 @@
+#!python
 # Copyright [2023] The Helx Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,35 +21,22 @@ import subprocess
 import tarfile
 
 import requests
-from setuptools import find_packages, setup
-from setuptools.command.install import install
-from setuptools.command.develop import develop
 
+from .logging import get_logger
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 REQUIREMENTS_PATH = "requirements.txt"
 MUJOCO_ROOT = os.path.join(os.path.expanduser("~"), ".mujoco")
 ATARI_ROOT = os.path.join(os.path.expanduser("~"), ".atari")
-BOLD = "\033[1m"
-DARK_GREY = "\033[1;30m"
-END = "\033[0m"
 
 
-logger = logging.getLogger("helx setup")
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(
-    logging.Formatter(
-        BOLD + DARK_GREY + "%(asctime)s | %(name)s | %(levelname)s | %(message)s" + END
-    )
-)
-logger.addHandler(ch)
+logging = get_logger()
 
 
 def _download_url(url, out_path, chunk_size=128):
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
     r = requests.get(url, stream=True)
-    logger.info("Downloading {} into {}".format(url, out_path))
+    logging.info("Downloading {} into {}".format(url, out_path))
     with open(out_path, "wb") as fd:
         chunks = 0
         for chunk in r.iter_content(chunk_size=chunk_size):
@@ -56,7 +44,7 @@ def _download_url(url, out_path, chunk_size=128):
             fd.write(chunk)
 
 
-def _download_mujoco210():
+def download_mujoco210():
     # example_url: "https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz"
     base_url = "https://mujoco.org/download"
     mujoco_version = "mujoco210"
@@ -90,7 +78,7 @@ def _download_mujoco210():
     _download_url(url, out_path)
 
     # unzip mujoco
-    logger.info("Extracting {} into {}".format(out_path, MUJOCO_ROOT))
+    logging.info("Extracting {} into {}".format(out_path, MUJOCO_ROOT))
     with tarfile.open(out_path, "r:gz") as tar_ref:
         tar_ref.extractall(MUJOCO_ROOT)
     os.remove(out_path)
@@ -101,7 +89,7 @@ def _download_mujoco210():
     _download_url(key_url, key_filepath)
 
 
-def _download_mujoco_dm_control():
+def download_mujoco_dm_control():
     # example url: https://github.com/deepmind/mujoco/releases/download/2.3.1/mujoco-2.3.1-linux-x86_64.tar.gz
 
     # get operating system
@@ -137,13 +125,13 @@ def _download_mujoco_dm_control():
     _download_url(url, out_path)
 
     # untar mujoco
-    logger.info("Extracting {} into {}".format(out_path, MUJOCO_ROOT))
+    logging.info("Extracting {} into {}".format(out_path, MUJOCO_ROOT))
     with tarfile.open(out_path, "r:gz") as tar_ref:
         tar_ref.extractall(MUJOCO_ROOT)
     os.remove(out_path)
 
 
-def _download_atari_roms():
+def download_atari_roms():
     out_path = os.path.join(ATARI_ROOT, "roms.rar")
     out_dir = os.path.dirname(out_path)
     if os.path.exists(out_dir) and len(os.listdir(out_dir)) > 0:
@@ -154,7 +142,7 @@ def _download_atari_roms():
     _download_url(url, out_path)
 
     # extract the file
-    logger.info("Extracting {} into {}".format(out_path, out_dir))
+    logging.info("Extracting {} into {}".format(out_path, out_dir))
     with tarfile.open(out_path, "r:gz") as tar_ref:
         tar_ref.extractall(out_dir)
 
@@ -167,15 +155,10 @@ def _download_atari_roms():
         Please install helx first and try download the extra requirements again".format(
             e
         )
-        logger.error(msg)
+        logging.error(msg)
 
 
-def _get_version():
-    filepath = os.path.join(CURRENT_DIR, "helx", "version.py")
-    with open(filepath) as f:
-        for line in f:
-            if line.startswith("__version__") and "=" in line:
-                version = line[line.find("=") + 1 :].strip(" '\"\n")
-                if version and isinstance(version, str):
-                    return version
-    raise ValueError("`__version__` not defined in {}".format(filepath))
+def download_all():
+    download_atari_roms()
+    download_mujoco210()
+    download_mujoco_dm_control()
