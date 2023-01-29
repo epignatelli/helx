4c43ab2ebf2ad1a6edf9f82fd93a61c6f280356b
“Eduardo <edu.pignatelli@gmail.com>
2023-01-29 02:10:25 +0000

    reorder import

--- helx/__init__.py
+++ helx/__init__.py
@@ -12,22 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import (
-    agents,
-    environment,
-    experiment,
-    flags,
-    image,
-    logging,
-    mdp,
-    memory,
-    networks,
-    preprocess,
-    random,
-    spaces,
-    stax,
-)
-
 # Make sure that the gym registry is populated
 # with exteral environments.
-import procgen, minigrid, gym_minigrid
+import gym_minigrid
+import minigrid
+import procgen
+
+from . import (agents, environment, experiment, flags, image, logging, mdp,
+               memory, networks, preprocess, random, spaces, stax)
